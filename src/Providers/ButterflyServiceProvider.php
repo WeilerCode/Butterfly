8b5f63b4cf9b5afb607e9ddf8ffe45c6d41c7d78
Weiler <wp31488341@vip.qq.com>
2017-12-07 14:42:45 +0800

    修复路劲错误

--- src/Providers/ButterflyServiceProvider.php
+++ src/Providers/ButterflyServiceProvider.php
@@ -16,9 +16,9 @@ class ButterflyServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__.'/Config/butterfly.php' => config_path('butterfly.php'),
+            __DIR__.'/../Config/butterfly.php' => config_path('butterfly.php'),
         ]);
-        $this->loadRoutesFrom(__DIR__.'/routes.php');
+        $this->loadRoutesFrom(__DIR__.'/../routes.php');
     }
 
     public function register()
