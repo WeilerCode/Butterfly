dbe5c7040810222d697b0ba466dfb785333460bb
Weiler <wp31488341@vip.qq.com>
2017-12-12 10:22:41 +0800

    版权等相关信息修改

--- src/Views/admin/layout/admin.blade.php
+++ src/Views/admin/layout/admin.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>AdminLTE 2 | Dashboard</title>
+    <title>Butterfly | Dashboard</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.7 -->
@@ -510,10 +510,9 @@
     <!-- /.content-wrapper -->
     <footer class="main-footer">
         <div class="pull-right hidden-xs">
-            <b>Version</b> 2.4.0
+            <b>Version</b> alpha
         </div>
-        <strong>Copyright &copy; 2014-2016 <a href="https://adminlte.io">Almsaeed Studio</a>.</strong> All rights
-        reserved.
+        <strong>Create by <a href="mailto:weiler.china@gmail.com">Weiler</a> & <a href="https://adminlte.io">Almsaeed Studio</a>.</strong>
     </footer>
     <!-- Add the sidebar's background. This div must be placed
          immediately after the control sidebar -->
