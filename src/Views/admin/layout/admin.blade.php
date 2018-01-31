82840db343d74b655cfc215ee6a340b26828f3d0
Weiler <wp31488341@vip.qq.com>
2018-01-31 16:29:28 +0800

    修改logo链接指向

--- src/Views/admin/layout/admin.blade.php
+++ src/Views/admin/layout/admin.blade.php
@@ -37,7 +37,7 @@
 
     <header class="main-header">
         <!-- Logo -->
-        <a href="" class="logo">
+        <a href="{{ route('admin-index') }}" class="logo">
             <!-- mini logo for sidebar mini 50x50 pixels -->
             <span class="logo-mini"><b>BF</b></span>
             <!-- logo for regular state and mobile devices -->
