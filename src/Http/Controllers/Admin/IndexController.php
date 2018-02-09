45e6e342c28463d472a0ecfe3bc79c1c90e1964c
Weiler <wp31488341@vip.qq.com>
2018-02-09 11:50:05 +0800

    增加后台主页的状态显示

--- src/Http/Controllers/Admin/IndexController.php
+++ src/Http/Controllers/Admin/IndexController.php
@@ -4,11 +4,24 @@ namespace Weiler\Butterfly\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use Weiler\Butterfly\Http\Controllers\AdminController;
+use Weiler\Butterfly\Models\AdminLog;
+use Weiler\Butterfly\Models\User;
 
 class IndexController extends AdminController
 {
     public function index(Request $request)
     {
-        return view('butterfly::admin.index');
+        // 硬盘使用率
+        $dt = round(@disk_total_space(".") / (1024*1024*1024),3); //总
+        $df = round(@disk_free_space(".") / (1024*1024*1024),3);  //可用
+        $du = $dt-$df;  //已用
+        $hdPercent = (floatval($dt)!=0) ? round($du/$dt*100,2) : 0;
+        // 会员总数
+        $memberNum = User::where('type', 'member')->count();
+        // 已验证会员数
+        $verifyMemberNum = User::where('type', 'member')->where('verify', 1)->count();
+        // 后台日志总数
+        $logNum = AdminLog::count();
+        return view('butterfly::admin.index')->with(['memberNum' => $memberNum, 'verifyMemberNum' => $verifyMemberNum, 'logNum' => $logNum, 'hdPercent' => $hdPercent]);
     }
 }
--- src/Views/admin/index.blade.php
+++ src/Views/admin/index.blade.php
@@ -2,8 +2,65 @@
 
 @section('content')
     <div class="row">
-        <div class="col-md-12">
-            <p>占位符</p>
+        <div class="col-lg-3 col-xs-6">
+            <!-- small box -->
+            <div class="small-box bg-yellow">
+                <div class="inner">
+                    <h3>{{ $memberNum }}</h3>
+
+                    <p>会员总数</p>
+                </div>
+                <div class="icon">
+                    <i class="fa fa-user-secret"></i>
+                </div>
+                <a href="{{ route('admin-member-member') }}" class="small-box-footer">详细信息 <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
         </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+            <!-- small box -->
+            <div class="small-box bg-green">
+                <div class="inner">
+                    <h3>{{ $verifyMemberNum }}</h3>
+
+                    <p>已验证会员</p>
+                </div>
+                <div class="icon">
+                    <i class="fa fa-user-plus"></i>
+                </div>
+                <a href="{{ route('admin-member-member') }}" class="small-box-footer">详细信息 <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+            <!-- small box -->
+            <div class="small-box bg-aqua">
+                <div class="inner">
+                    <h3>{{ $logNum }}</h3>
+
+                    <p>后台日志</p>
+                </div>
+                <div class="icon">
+                    <i class="fa fa-terminal"></i>
+                </div>
+                <a href="{{ route('admin-manage-log') }}" class="small-box-footer">详细信息 <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+            <!-- small box -->
+            <div class="small-box bg-red">
+                <div class="inner">
+                    <h3>{{ $hdPercent }} <sup style="font-size: 20px">%</sup></h3>
+
+                    <p>磁盘使用率</p>
+                </div>
+                <div class="icon">
+                    <i class="fa fa-hdd-o"></i>
+                </div>
+                <div style="padding: 3px 0;">&nbsp;</div>
+            </div>
+        </div>
+        <!-- ./col -->
     </div>
 @endsection
